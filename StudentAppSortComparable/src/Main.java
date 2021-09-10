01f234f24a0d945a0d3f75968d9d4e3313765f9c
kimgunhwi <21700063@handong.edu>
2021-09-10 19:40:17 +0900

    comparable commit

---
+++ StudentAppSortComparable/src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+	public static void main(String[] args) {
+		List<Student> al = new ArrayList<Student>();
+		
+		//append student
+		Student s1 = new Student(101, "Kim", 23);
+		al.add(s1);
+		Student s2 = new Student(102, "Choi", 21);
+		al.add(s2);
+		al.add(new Student(103, "Park", 25));
+		
+		al.add(new Student(104, "Bin", 20));
+		al.add(new Student(105, "You", 20));
+		al.add(new Student(106, "Cha", 20));
+		al.add(new Student(107, "Ha", 22));
+		al.add(new Student(108, "Ahn", 25));
+		al.add(new Student(109, "Lee", 20));
+		al.add(new Student(110, "Seo", 21));
+		al.add(new Student(111, "Oh", 22));
+		al.add(new Student(112, "Jo", 23));
+		
+		
+		Collections.sort(al);
+		System.out.println("Student List (ordered by name)");
+		for(Student s : al) {System.out.println(s.toString());}
+		System.out.println();
+		
+		
+		System.out.println("Student List (reverse ordered by name)");
+		Collections.sort(al, Collections.reverseOrder());
+		for(int i=0; i<al.size(); i++) {
+			System.out.println(al.get(i).toString());
+		}
+	}
+
+}
---
+++ StudentAppSortComparable/src/Student.java
@@ -0,0 +1,22 @@
+public class Student implements Comparable<Student>{
+	int no;
+	String name;
+	int age;
+
+	public Student(int no, String name, int age) {
+		super();
+		this.no = no;
+		this.name = name;
+		this.age = age;
+	}
+	
+	@Override
+	public String toString() {
+		return "Student [no=" + no + ", name=" + name + ", age=" + age + "]";
+	}
+
+	@Override
+	public int compareTo(Student o) {
+		return name.compareTo(o.name);
+	}
+}
